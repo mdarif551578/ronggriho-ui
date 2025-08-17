57d473a79cd8b4a8da11a6cc9afd309749d465f7
Md Arif <mda510624@gmail.com>
2025-08-17 09:43:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/products/[slug]/product-details-client.tsx
+++ src/app/products/[slug]/product-details-client.tsx
@@ -14,6 +14,7 @@ import { useCart } from '@/hooks/use-cart';
 import { useToast } from '@/hooks/use-toast';
 import { useWishlist } from '@/hooks/use-wishlist';
 import { cn } from '@/lib/utils';
+import { Label } from '@/components/ui/label';
 
 interface ProductDetailsClientProps {
     product: Product;
