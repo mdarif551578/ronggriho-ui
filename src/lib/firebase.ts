421ca3acb72ebae28e0112dc5d145f727ca1893c
Md Arif <mda510624@gmail.com>
2025-08-17 15:37:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAnalytics, isSupported }from "firebase/analytics";
@@ -19,10 +20,7 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const firestore = getFirestore(app);
 const auth = getAuth(app);
-let analytics;
+const analytics = typeof window !== 'undefined' && isSupported().then(yes => yes ? getAnalytics(app) : null);
 
-if (typeof window !== 'undefined' && isSupported()) {
-  analytics = getAnalytics(app);
-}
 
 export { app, firestore, auth, analytics };
